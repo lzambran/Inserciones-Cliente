aadde58917e4fbc4f300a8a481cfcf9c70829646
lzambran <76109631+lzambran@users.noreply.github.com>
2021-03-02 08:24:24 -0500

    Insercion de de datos para el trigger cliente

---
+++ Inserciones Trigger Cliente.sql
@@ -0,0 +1,7 @@
+--Insercion para el trigger cliente
+
+insert into Cliente values (1,'Luis','Zambrano','Crucita','0978716971','1314487222');
+insert into Cliente values (2,'Pedro','Camacho','Portoviejo','0998754123','1303698742');
+insert into Cliente values (3,'Maria','Mendoza','Manta','0984563210','1317896524');
+insert into Cliente values (4,'Luis','Alcivar','Chone','0998756321','1316321478');
+
